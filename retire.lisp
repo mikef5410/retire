eac017e1bc381474e9cdcab2357e9397289cfd99
Mike Ferrara <mikef@broadcom.com>
2023-06-01 15:57:56 -0700

    option parsing wip

--- retire.lisp
+++ retire.lisp
@@ -1,4 +1,5 @@
 ;;;; retire.lisp
+;;;; see: https://stevelosh.com/blog/2021/03/small-common-lisp-cli-programs/
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (ql:quickload '(:with-user-abort :adopt ) :silent t))
 
@@ -7,6 +8,34 @@
   (:export :toplevel *ui*))
 
 (in-package #:retire)
+
+(defparameter *help* nil)
+(defparameter *short* nil)
+
+(defparameter *option-short*
+  (adopt:make-option 'short
+                     :long "short"
+                     :short #\s
+                     :help "short output"
+                     :reduce (constantly t)))
+
+(defparameter *option-help*
+  (adopt:make-option 'help
+                     :long "help"
+                     :short #\h
+                     :help "Display help and exit."
+                     :reduce (constantly t)))
+
+(defparameter *ui*
+  (adopt:make-interface
+    :name "retire"
+    :summary "countdown to retirement"
+    :usage "[OPTIONS] ..."
+    :help "How many days/hours/working-days to retirement?"
+    :contents (list
+                *option-help*
+                *option-short*)))
+
 
 ;;;; Errors ------------------------------------------------------
 (define-condition user-error (error) ())
@@ -32,11 +61,15 @@
         (multiple-value-setq (hours sec) (floor hours (* 60 60)))
         (setf workdays (floor (- (/ (* days 5) 7) (* years 25))))
         (when (> to-day 4) (setf workdays (+ workdays (- to-day 4)))) ;Sat or Sun? 1 or 2 days back
-        (format t "Retire in ~d days ~d hours! (~d work days)~%" days hours workdays)))))
+        (unless *short*
+          (format t "Retire in ~d days ~d hours! (~d work days)~%" days hours workdays))
+        (when *short*
+          (format t "~d/~d (~d)~%" days hours workdays))
+        (values days hours workdays)))))
 
 ;;;; Run ---------------------------------------------------------
 (defun run ()
-  (calcRetire))
+  (calcRetire ))
 
 ;;;; User Interface ----------------------------------------------
 (defmacro exit-on-ctrl-c (&body body)
@@ -50,12 +83,16 @@
 
 (defun toplevel ()
   (sb-ext:disable-debugger)
-  ;; (exit-on-ctrl-c
-  ;;   (multiple-value-bind (arguments options) (adopt:parse-options-or-exit *ui*)
-  ;;     … ; Handle options.
-  ;;     (handler-case (run )
-  ;;       (user-error (e) (adopt:print-error-and-exit e)))))
-  (run)
-  )
+  (exit-on-ctrl-c
+    (multiple-value-bind (arguments options) (adopt:parse-options-or-exit *ui*)
+      (car arguments)
+     (when (gethash 'help options)
+       (adopt:print-help-and-exit *ui*))
+     (when (gethash 'short options)
+       (setf *short* t))
+     ;;(handler-case (run )
+     ;;  (user-error (e) (adopt:print-error-and-exit e)))
+     (run))
+     ))
 
 
